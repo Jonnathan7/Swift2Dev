3c1a94cc3cf0a4119cc6aff2c2905cd97bf4dbc6
Jonnathan De La Barra <justindelabarra@gmail.com>
2017-01-17 11:09:35 -0500

    Initial Commit

---
+++ Xcode2Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode2Sample
+//
+//  Created by Jonnathan De La Barra on 17/01/17.
+//  Copyright © 2017 Jonnathan De La Barra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
